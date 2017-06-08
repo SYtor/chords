a0ec6ea0b1bec506772cf7232736ccd03eae22a2
SYtor <shulyakyarik@gmail.com>
2017-06-08 19:02:19 +0000

    welcome fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @updates = Chord.order('createded_at DESC').limit(20)
+    @updates = Chord.order('created_at DESC').limit(20)
     @top = Chord.order('views DESC').limit(20)
   end
 end
